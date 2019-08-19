6d083bcb3a55f803915b5dc4141e34e3f52ce87b
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-19 01:03:45 +0100

    Default TextfieldHealth

--- src/objects/TextfieldHealth.svelte
+++ src/objects/TextfieldHealth.svelte
@@ -4,9 +4,33 @@
 
 <style>
   .textfield-health {
+    display: block;
+    position: relative;
+    background-color: var(--color-gray-light);
+    color: var(--color-black-disabled);
     font-size: var(--size-third);
     font-weight: normal;
+    border-radius: var(--gap-radius) var(--gap-radius) 0 0;
+    padding: var(--gap-second);
+  }
+
+  .textfield-health::after {
+    content: '';
+    background-color: var(--color-black-disabled);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: var(--size-border);
+  }
+
+  .textfield-health > .input {
+    background-color: transparent;
+    width: 100%;
+    height: 100%;
   }
 </style>
 
-<input type="text" class="textfield-health" {placeholder} />
+<label class="textfield-health">
+  <input class="input" type="text" {placeholder} />
+</label>
