db697129d280060492eb309213b676214693f6d7
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-25 00:38:34 +0100

    Created the SubtitleHealth

---
+++ src/stories/objects/SubtitleHealthView.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Section from '../util/Sections.svelte'
+  import SubtitleHealth from '../../objects/SubtitleHealth.svelte'
+
+  export let content
+  export let dark = false
+
+  const config = {
+    title: 'Subtitle Health',
+    dark,
+    description:
+      'Esse object usamos para representar subtítulo, usamos a tag h2',
+    exampleImport: `import SubtitleHealth from './SubtitleHealth.svelte`,
+    exampleUse: `<SubtitleHealth content='${content}' dark={${dark}} />`,
+  }
+</script>
+
+<Section {...config}>
+  <SubtitleHealth {content} {dark} />
+</Section>
